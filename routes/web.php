00eb72efb0c5b8b189df01e002c266115357e30d
Sander Borret <sander.borret@student.kdg.be>
2017-01-23 22:37:48 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ArticlesController@index');
+Route::get('comments/{article}', 'ArticlesController@show');
+
+Route::get('article/add', 'ArticlesController@add');
+Route::post('article/add', 'ArticlesController@store');
+
+Route::get('article/edit/{article}', 'ArticlesController@edit');
+Route::patch('article/edit/{article}', 'ArticlesController@update');
+
+Route::patch('vote/up/{article}', 'ArticlesController@upvote');
+Route::patch('vote/down/{article}', 'ArticlesController@downvote');
+
+Route::post('comments/add/{article}', 'CommentsController@store');
+
+Route::get('comments/edit/{comment}', 'CommentsController@edit');
+Route::patch('comments/edit/{comment}', 'CommentsController@update');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
